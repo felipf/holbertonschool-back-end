b077a77f3f3a8a1e766514929dab0a784b4d1117
felipf <6341@holbertonstudents.com>
2023-08-25 17:19:37 +0000

    fix

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """gather data from api"""
-import requests
+from requests import get
 from sys import argv
 
 if __name__ == "__main__":
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     user_ep = f"{base_url}/users/{user_id}"
     todo_ep = f"{base_url}/todos/?userId={user_id}"
 
-    user_data = requests.get(user_ep).json()
+    user_data = get(user_ep).json()
     user_name = user_data.get("name")
-    todo_data = requests.get(todo_ep).json()
-    
+    todo_data = get(todo_ep).json()
+
     completed_tasks = []
 
     for task in todo_data:
@@ -26,6 +26,6 @@ if __name__ == "__main__":
 
     print(f"Employee {user_name} is done with tasks"
           f"({num_completed_tasks}/{total_tasks}):")
-    
+
     for task_title in completed_tasks:
         print(f"\t {task_title}")
