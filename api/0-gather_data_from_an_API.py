9bd571a4c5951a0ec2ade3bfa512e919c42d85ea
felipf <felipereiraforte@gmail.com>
2023-08-25 11:12:59 -0700

    task and fixes

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
 """gather data from api"""
-from requests import get
+import requests
 from sys import argv
 
-if __name__ == "__main__":
-    """gather data from api"""
-    user_id = argv[1]
 
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_ep = f"{base_url}/users/{user_id}"
-    todos_ep = f"{base_url}/todos/?userId={user_id}"
-
-    user_data = get(user_ep).json()
-    user_name = user_data.get("name")
-    todos_data = get(todos_ep).json()
-
-    completed_tasks = []
-
-    for task in todos_data:
-        if task["completed"]:
-            completed_tasks.append(task["title"])
-
-    total_tasks = len(todos_data)
-    num_completed_tasks = len(completed_tasks)
-
-    print(f"Employee {user_name} is done with tasks"
-          f"({num_completed_tasks}/{total_tasks}):")
-
-    for task_title in completed_tasks:
-        print(f"\t {task_title}")
+if __name__ == '__main__':
+    user_page = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".
+        format(argv[1])).json()
+    todo_page = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".
+        format(argv[1])).json()
+    titles = []
+    for task in todo_page:
+        if task.get('completed') is True:
+            titles.append(task.get('title'))
+    print("Employee {} is done with tasks({}/{}):".
+          format(user_page.get('name'), len(titles), len(todo_page)))
+    print("\n".join("\t {}".format(task) for task in titles))
